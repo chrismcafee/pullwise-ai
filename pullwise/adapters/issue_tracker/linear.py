9e215ff9e2971191606f4e61fb3aec6b78230fa8
Chris McAfee <chris.mcafee@producepay.com>
2025-07-11 19:25:39 -0600

    fmt

--- pullwise/adapters/issue_tracker/linear.py
+++ pullwise/adapters/issue_tracker/linear.py
@@ -1,6 +1,5 @@
 from pullwise.ports.issue_tracker_port import IssueTrackerPort
 
-
 class Linear(IssueTrackerPort):
     def get_issue(self, issue_key: str):
         # TODO: Implement integration with Linear
--- pullwise/adapters/issue_tracker/youtrack.py
+++ pullwise/adapters/issue_tracker/youtrack.py
@@ -1,6 +1,5 @@
 from pullwise.ports.issue_tracker_port import IssueTrackerPort
 
-
 class Youtrack(IssueTrackerPort):
     def get_issue(self, issue_key: str):
         # TODO: Implement integration with Youtrack
